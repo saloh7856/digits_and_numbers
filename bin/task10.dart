79dce95e0d78b57751f34e3bf6052a25ec1f6979
saloh7856 <salohiddinberdiyorov2002@gmail.com>
2022-06-20 13:07:13 -0700

    dan

--- bin/task10.dart
+++ bin/task10.dart
@@ -8,4 +8,11 @@
   Returns:
       return answer
   */
-void main() {}
+int func(int number) {
+  return ((number~/1000)*((number~/100)%10)*((number~/10)%10)*(number%10));
+}
+void main(){
+
+  print(func(1234));
+}
+
